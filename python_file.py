e3b91bfcc729dc032c3310f035b21ef0d8748d3e
Nick <nickjboyer@gmail.com>
2025-01-18 15:38:13 -0500

    something to show

--- python_file.py
+++ python_file.py
@@ -1,5 +1,29 @@
-# i don't understand
-# please work
+
+#### this is the line that works to run streamlit
+#py -m streamlit run python_file.py
+
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.info("this is some test text")
 
 
-print('hello there')
+with col2:
+    st.title("Nick Boyer")
+    content = """
+        I'm so cool
+            """
+    
+    st.info(content)
+
+
+content2 = """
+    add some sort of footer
+"""
+st.write(content2)
+
+
