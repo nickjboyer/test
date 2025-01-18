aeedfb53490f0a05a991e571fe32b74c775887b2
Nick <nickjboyer@gmail.com>
2025-01-18 16:50:24 -0500

    dice randomizer

--- python_file.py
+++ python_file.py
@@ -3,27 +3,29 @@
 #py -m streamlit run python_file.py
 
 import streamlit as st
+from random import randint
 
-st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
-
-with col1:
-    st.info("this is some test text")
+col1, col2, col3 = st.columns([1,2,1])
 
 
 with col2:
-    st.title("Nick Boyer")
-    content = """
-        I'm so cool
-            """
+    st.title("Dice Randomizer")
+    num_dice = st.selectbox("Select the number of D6 dice to roll:", options=range(1, 6))
+
     
-    st.info(content)
-
-
-content2 = """
-    add some sort of footer
-"""
-st.write(content2)
+    button = st.button("Roll Dice")
+    
+    if button:
+        results = [randint(1, 6) for _ in range(num_dice)]
+        st.write(f"You rolled {num_dice} dice: ")
+        result_str = ""
+        result_num = 0
+        for result in results:
+            result_num += result
+            result_str += str(result)+", "
+        
+        st.write(result_str[:-2])
+        st.write(f"TOTAL = {result_num}")
 
 
